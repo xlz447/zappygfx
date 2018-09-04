065a4b3f7e49aa4b3fbc29e9cf5c87b5ba5fbb45
Yayutan <chuangmic25@gmail.com>
2018-09-03 20:30:01 -0500

    adding event handling fixed the problem

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from grid import *
 
 GAME_OVER = False
 
+for row in range(MAPHEIGHT):
+	for column in range(MAPWIDTH):
+		DISPLAYSURFACE.blit(TEXTURES[GRID[row][column]], (column*TILESIZE, row*TILESIZE))
 while not GAME_OVER:
-	for row in range(MAPHEIGHT):
-		for column in range(MAPWIDTH):
-			DISPLAYSURFACE.blit(TEXTURES[GRID[row][column]], (column*TILESIZE, row*TILESIZE))
-	pygame.display.update()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			GAME_OVER = True
+	pygame.display.flip() # flip updates the whole screen
