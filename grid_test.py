93f1892763a8995f5e87f05cb1fdcb5efd0a9385
Yayutan <chuangmic25@gmail.com>
2018-09-07 22:52:30 -0500

    added a test run using the new class with randomized item locaiton

--- grid_test.py
+++ grid_test.py
@@ -1,11 +1,11 @@
-import random
+import random, pygame
 
 class Grid:
 	def __init__(self):
 		self.background = None
 		self.items = [] #[{x0, y0, 0}, {x1, y1,1}, ...., {x6, y6,1}] # will always be length 7
 		self.players = [] #[{x0, y0, image}, {x1, y1, image},......] # might be empty
-		for i in range(0, 7):
+		for i in range(0, 6):
 			self.items.append([random.random(), random.random(), 0]) # set up the random xy coordinates and start with no item
 	
 	
@@ -24,20 +24,105 @@ class Grid:
 	"""
 	def setup(self, img, items, players):
 		self.background = img
-		for i in range(6, -1, -1):
-			self.items[i][1] = items % 2
+		for i in range(5, -1, -1):
+			self.items[i][2] = items % 2
 			items = items / 2
-			
 		# test items
-		for j in range(0, 7):
-			print("Item " + str(j) + " is " + str(self.items[j][1]))
+#		for j in range(0, 7):
+#			print("Item " + str(j) + " is " + str(self.items[j][1]))
 
 		# set up players
 		self.players = []
 
-new_grid = Grid()
-new_grid.setup(None, 127, [])
-print("Should be 1 1 1 1 1 1 1")
+		
+		
+		
+# this is just for testing
+# TILES
+BRICKMOSS = 0
+GRASS = 1
+BRICK = 2
+GRASSFLOWER = 3
+GRASSSTONE = 4
+PLASTER = 5
+SOIL = 6
 
-new_grid.setup(None, 111, [])
-print("Should be 1 1 0 1 1 1 1")
+DERAUMERE = 0
+LINEMATE = 1
+MENDIANE = 2
+PHIRAS = 3
+SIBUR = 4
+THYSTAME = 5
+
+TEXTURES = {
+    BRICKMOSS: pygame.image.load('./textures/floor/brickmoss.jpg'),
+    GRASS: pygame.image.load('./textures/floor/grass.jpg'),
+	BRICK: pygame.image.load('./textures/floor/brick.jpg'),
+    GRASSFLOWER: pygame.image.load('./textures/floor/grassflower.jpg'),
+    GRASSSTONE: pygame.image.load('./textures/floor/grassstone.jpg'),
+    PLASTER: pygame.image.load('./textures/floor/plaster.jpg'),
+	SOIL: pygame.image.load('./textures/floor/soil.jpg')
+}
+
+ITEMS = {
+	DERAUMERE:
+	pygame.image.load('./textures/item/deraumere20.png'),
+	LINEMATE:
+	pygame.image.load('./textures/item/linemate20.png'),
+	MENDIANE:
+	pygame.image.load('./textures/item/mendiane20.png'),
+	PHIRAS:
+	pygame.image.load('./textures/item/phiras20.png'),
+	SIBUR:
+	pygame.image.load('./textures/item/sibur20.png'),
+	THYSTAME:
+	pygame.image.load('./textures/item/thystame20.png')
+}
+
+GRIDS = []
+
+# testing message
+FAKEMSG_ITEM = "55,53,23,30,32,20\n" + "53,23,32,10,20,30\n" + "55,53,23,20,30,20\n" + "25,23,22,10,30,5\n" + "5,30,25,3,22,2"
+#FAKEMSG_ITEM = "2,3\n" + "53,23"
+
+FAKEMSG_PLY = ""
+ROW_ITEM = FAKEMSG_ITEM.split("\n")
+NUM_ROW = len(ROW_ITEM)
+NUM_COL = len(ROW_ITEM[0].split(","))
+
+# setup pygame, default max win is 500 * 500
+TILESIZE = min([500 / NUM_ROW, 500 / NUM_COL])
+ITEMSIZE = 20
+pygame.init()
+pygame.display.set_caption('testing')
+DISPLAYSURFACE = pygame.display.set_mode((NUM_COL * TILESIZE, NUM_ROW * TILESIZE))
+
+for i in TEXTURES:
+	pygame.transform.scale(TEXTURES[i], (TILESIZE, TILESIZE))
+
+
+# setup items
+for row in range(0, NUM_ROW):
+	GRIDS.append([])
+	COL_ITEM = ROW_ITEM[row].split(",")
+	if len(COL_ITEM) != NUM_COL:
+		raise ValueError("Inconsistent column length at row " + str(row))
+	for col in range(0, NUM_COL):
+		new_grid = Grid()
+		new_grid.setup(TEXTURES[SOIL], int(COL_ITEM[col]), [])
+		GRIDS[row].append(new_grid)
+		
+GAMEOVER = False
+while GAMEOVER != True:
+	for event in pygame.event.get():
+		keys = pygame.key.get_pressed()
+        if event.type == pygame.QUIT:
+			GAMEOVER = True
+			
+	for row in range(NUM_ROW):
+		for column in range(NUM_COL):
+			DISPLAYSURFACE.blit(GRIDS[row][column].background, (column*TILESIZE, row*TILESIZE))
+			for i in range(6):
+				if GRIDS[row][column].items[i][2] is 1:
+					DISPLAYSURFACE.blit(ITEMS[i], (column*TILESIZE + (TILESIZE - ITEMSIZE) * GRIDS[row][column].items[i][0], row*TILESIZE + (TILESIZE - ITEMSIZE) * GRIDS[row][column].items[i][1]))
+	pygame.display.update()
